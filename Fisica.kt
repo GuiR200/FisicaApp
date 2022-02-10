9434ef4ea3f72632036f66ae2129c8d86f12389c
unknown <you@example.com>
2022-02-09 21:14:34 -0300

    Atualizado

--- Fisica.kt
+++ Fisica.kt
@@ -11,7 +11,7 @@ class Fisica {
 
     fun calcularVelocidadeMedia(velocidadeInicial: Float, velocidadeFinal: Float, tempoInicial: Float, tempoFinal: Float) = velocidadeFinal - velocidadeInicial / tempoFinal - tempoInicial
 
-    fun calcularMRU(sentidoInicial: Float, velocidade: Float, tempoFinal: Float, tempoInicial : Float) = sentidoInicial + velocidade * tempoFinal - tempoInicial
+    fun calcularMRU(sentidoInicial: Float, velocidade: Float, tempoFinal: Float, tempoInicial : Float) = sentidoInicial + velocidade * (tempoFinal - tempoInicial)
 
     fun calcularMRUV(sentidoInicial : Float,velocidade : Float, tempo : Float, aceleracao : Float, tempoDaDivisao : Double) = sentidoInicial + velocidade * tempo + (aceleracao + tempoDaDivisao.pow(2) / 2)
 }
